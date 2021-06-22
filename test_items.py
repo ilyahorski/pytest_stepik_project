2ac8b2aa09c66194396cebdb4a35043e1dee8996
ilyahorski <gorskiyia@gmail.com>
2021-06-22 15:29:33 +0300

    Test for review in 3.1.11

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import pytest
+import time
+
+
+def test_links(browser):
+    browser.get(' http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    time.sleep(5)
+    basket = browser.find_element_by_id('add_to_basket_form')
+    assert basket, 'basket not found'
